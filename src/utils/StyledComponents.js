7da141969089083fa39f418808f8da419096bd77
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-07-19 13:08:43 +0530

    Create StyledComponents.js

---
+++ src/utils/StyledComponents.js
@@ -0,0 +1,12 @@
+import { styled } from 'nativewind';
+import { View, Text, Button, Image, ScrollView, FlatList, SafeAreaView, TouchableOpacity, StatusBar } from 'react-native';
+
+export const StyledView = styled(View);
+export const StyledText = styled(Text);
+export const StyledButton = styled(Button);
+export const StyledImage = styled(Image);
+export const StyledScrollView = styled(ScrollView);
+export const StyledFlatList = styled(FlatList);
+export const StyledSafeAreaView = styled(SafeAreaView);
+export const StyledTouchableOpacity = styled(TouchableOpacity);
+export const StyledStatusBar = styled(StatusBar);
