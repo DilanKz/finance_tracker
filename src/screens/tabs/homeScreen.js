5a17c3aa6eefe8b170e3c4556432acbe6c50557a
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-07-20 23:03:19 +0530

    Create homeScreen.js

---
+++ src/screens/tabs/homeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const HomeScreen = () => {
+    return (
+        <View className="flex-1 bg-gray-100 p-4">
+            <Text className="text-lg">Home Screen</Text>
+        </View>
+    );
+};
+
+export default HomeScreen;
