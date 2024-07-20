d7e335293e4da998bd0ac734efb361f9c518810f
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-07-20 23:03:28 +0530

    Create transactionScreen.js

---
+++ src/screens/tabs/transactionScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const TransactionScreen = () => {
+    return (
+        <View className="flex-1 bg-gray-100 p-4">
+            <Text className="text-lg">Transaction Screen</Text>
+        </View>
+    );
+};
+
+export default TransactionScreen;
