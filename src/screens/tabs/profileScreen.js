e359a6b21a37587f44e6f35f38dc8372462d763e
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-07-20 23:03:25 +0530

    Create profileScreen.js

---
+++ src/screens/tabs/profileScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const ProfileScreen = () => {
+    return (
+        <View className="flex-1 bg-gray-100 p-4">
+            <Text className="text-lg">Profile Screen</Text>
+        </View>
+    );
+};
+
+export default ProfileScreen;
