f1fed24d09517c8697e5e16aff9a2b2023cb9132
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-07-20 23:02:58 +0530

    Update register.js

--- src/components/boarding/register.js
+++ src/components/boarding/register.js
@@ -3,13 +3,15 @@ import React, {useState} from "react";
 
 // ** React Native Imports
 import {Text, TextInput, TouchableOpacity, View} from "react-native";
+import {useNavigation} from "@react-navigation/native";
 
 // ** Expo Icons
 import { Ionicons } from '@expo/vector-icons';
 import { AntDesign } from '@expo/vector-icons';
 import PasswordInput from "../inputs/passwordInput";
-import { TailwindProvider } from 'nativewind';
 export default function Register({setScreen}) {
+
+    const navigation = useNavigation()
 
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
@@ -18,12 +20,14 @@ export default function Register({setScreen}) {
     return (
         <View className={'w-screen'}>
 
-            <View className={'mt-16 w-full py-2 px-4 flex flex-row justify-between text-center'}>
-                <TouchableOpacity onPress={() => setScreen('boarding')}>
+            <View className={'mt-16 w-full py-2 px-4 flex flex-row text-center'}>
+                <TouchableOpacity
+                    onPress={() => setScreen('boarding')}
+                    className="absolute left-4 top-0 bottom-0 flex items-center justify-center z-10"
+                >
                     <AntDesign name="arrowleft" size={24} color="black" />
                 </TouchableOpacity>
-                <Text className={'text-xl font-bold text-black'}>Sign Up</Text>
-                <Text className={'text-xl text-black'}></Text>
+                <Text className="text-xl font-bold text-black flex-1 text-center">Sing Up</Text>
             </View>
 
             <View className={'w-screen py-2 px-3'}>
@@ -45,8 +49,10 @@ export default function Register({setScreen}) {
                 </View>
                 <PasswordInput value={password} onChange={setPassword} />
 
-                <TouchableOpacity className={'w-full flex items-center py-4 bg-customPurple rounded-xl mb-4'}>
-                    <Text className={'text-xl text-gray-100'}>Login</Text>
+                <TouchableOpacity className={'w-full flex items-center py-4 bg-customPurple rounded-xl mb-4'}
+                                  onPress={()=>navigation.navigate('main')}
+                >
+                    <Text className={'text-xl text-gray-100'}>Sign Up</Text>
                 </TouchableOpacity>
 
                 <View className="flex justify-center items-center flex-row mt-2">
