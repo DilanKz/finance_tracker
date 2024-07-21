9d204b742ce5fbb14568ea245464b5dd99ac91c9
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-07-21 13:12:47 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import OnboardScreen from "./src/screens/onBoardingScreen";
 import LandingScreen from "./src/screens/landingScreen";
+import MainScreen from "./src/screens/mainScreen";
 
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
@@ -12,6 +13,7 @@ export default function App() {
             <Stack.Navigator initialRouteName={'Loading'} screenOptions={{headerShown: false}}>
                 <Stack.Screen name="Loading" component={LandingScreen}/>
                 <Stack.Screen name="onBoard" component={OnboardScreen}/>
+                <Stack.Screen name="main" component={MainScreen}/>
             </Stack.Navigator>
         </NavigationContainer>
     );
