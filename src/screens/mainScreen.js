e9775a1bc25a21d0cf6ba4808cd0b650ff43742c
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-07-23 19:36:12 +0530

    Update mainScreen.js

--- src/screens/mainScreen.js
+++ src/screens/mainScreen.js
@@ -1,5 +1,5 @@
 // ** React Imports
-import React from 'react';
+import React, {useState} from 'react';
 
 // ** React Native Imports
 import { View, Text } from 'react-native';
@@ -13,31 +13,26 @@ import TransactionScreen from "./tabs/transactionScreen";
 import BudgetScreen from "./tabs/budgetScreen";
 import ProfileScreen from "./tabs/profileScreen";
 import CustomTabBar from "../components/manu/customTabBar";
+import {RouteProvider} from "../components/context/routeProvider";
 
-
-const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
-const TabNavigator = () => (
-    <Tab.Navigator screenOptions={{ headerShown: false }}>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Transaction" component={TransactionScreen} />
-        <Tab.Screen name="Budget" component={BudgetScreen} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
-    </Tab.Navigator>
-);
-
 export default function MainScreen () {
+
+    const [route, setRoute] = useState('Home');
+
     return (
-        <View className="flex-1">
-            <Stack.Navigator initialRouteName={'Home'}>
-                <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
-                <Stack.Screen name="Transaction" component={TransactionScreen} options={{ headerShown: false }} />
-                <Stack.Screen name="Budget" component={BudgetScreen} options={{ headerShown: false }} />
-                <Stack.Screen name="Profile" component={ProfileScreen} options={{ headerShown: false }} />
-            </Stack.Navigator>
-            <NavigationBar />
-        </View>
+        <RouteProvider>
+            <View className="flex-1">
+                <Stack.Navigator initialRouteName={'Home'}>
+                    <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+                    <Stack.Screen name="Transaction" component={TransactionScreen} options={{ headerShown: false }} />
+                    <Stack.Screen name="Budget" component={BudgetScreen} options={{ headerShown: false }} />
+                    <Stack.Screen name="Profile" component={ProfileScreen} options={{ headerShown: false }} />
+                </Stack.Navigator>
+                <NavigationBar />
+            </View>
+        </RouteProvider>
     );
 };
 
