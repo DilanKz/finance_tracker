ff64b0266735c6a6183632bbb76c2a85e293d645
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-07-26 11:03:49 +0530

    Update transactionDetailCard.js

--- src/components/transactions/transactionDetailCard.js
+++ src/components/transactions/transactionDetailCard.js
@@ -1,13 +1,15 @@
 import {Text, View} from "react-native";
 import TransactionCard from "./transactionCard";
 
+const arr = ['Shopping', 'Food', 'Travel', 'Health']
+
 const TransactionDetailCard = (props) => {
     return (
         <View className={'w-full'}>
             <Text className="text-2xl text-gray-800 font-semibold">{props.title}</Text>
-            <TransactionCard />
-            <TransactionCard />
-            <TransactionCard />
+            {arr.map((name,index) => (
+                <TransactionCard key={index} title={name} />
+            ))}
         </View>
     )
 }
