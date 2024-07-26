bd3b89eae59c1855376e0cd6cb154defa5ed4ddf
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-07-26 20:03:08 +0530

    Update profileScreen.js

--- src/screens/tabs/profileScreen.js
+++ src/screens/tabs/profileScreen.js
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native';
 const ProfileScreen = () => {
     return (
         <View className="flex-1 bg-gray-100 p-4">
-            <Text className="text-lg">Profile Screen</Text>
+            <Text className="text-lg pt-16">Profile Screen</Text>
         </View>
     );
 };
