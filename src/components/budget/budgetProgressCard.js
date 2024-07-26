ab89a730c361427e78e514f80f332ed4971f1276
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-07-26 13:47:33 +0530

    Create budgetProgressCard.js

---
+++ src/components/budget/budgetProgressCard.js
@@ -0,0 +1,41 @@
+import { Text, View } from "react-native";
+import * as Progress from 'react-native-progress';
+import {getCategoryByTitle} from "../../utils/constants";
+
+export const BudgetProgressCard = (props) => {
+    const category = getCategoryByTitle(props.title);
+    return (
+        <View className={'p-1 mb-2'}>
+            <View style={{
+                backgroundColor: 'white',
+                borderRadius: 12,
+                shadowColor: '#000',
+                shadowOffset: { width: 0, height: 2 },
+                shadowOpacity: 0.25,
+                shadowRadius: 4,
+                elevation: 5,
+                padding:8,
+            }}>
+                <View className='flex-row justify-between items-center mb-2'>
+                    <View className='flex-row items-center'>
+                        <View className={`${category.color} p-2 rounded-lg flex-row items-center justify-center mr-2`}>
+                            {category.iconSmall}
+                        </View>
+                        <Text className='text-lg font-semibold'>{category.title}</Text>
+                    </View>
+                    <Text className='text-lg'>-</Text>
+                </View>
+
+                <Text className='text-lg mb-2'>1200/2000</Text>
+
+                <Progress.Bar
+                    progress={100 / 120}
+                    width={null}
+                    color={category.colorHex}
+                    borderRadius={10}
+                    height={8}
+                />
+            </View>
+        </View>
+    );
+}
