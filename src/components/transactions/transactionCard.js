b3ecd40cbcbbbea4004fd00ae1df3d68cd28ec39
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-07-26 13:47:37 +0530

    Update transactionCard.js

--- src/components/transactions/transactionCard.js
+++ src/components/transactions/transactionCard.js
@@ -1,20 +1,20 @@
 import {Text, View} from "react-native";
-import { FontAwesome6 } from '@expo/vector-icons';
+import { FontAwesome6, MaterialCommunityIcons } from '@expo/vector-icons';
 import {getCategoryByTitle} from "../../utils/constants";
 const TransactionCard = (props) => {
 
-    const shoppingCategory = getCategoryByTitle(props.title);
+    const category = getCategoryByTitle(props.title);
 
     return (
         <View className="w-full my-4 flex-row px-4 ">
-            <View className={`${shoppingCategory.color} w-20 h-20 rounded-2xl flex-row justify-center items-center`}>
-                {shoppingCategory.icon}
+            <View className={`${category.color} w-20 h-20 rounded-2xl flex-row justify-center items-center`}>
+                {category.icon}
             </View>
             <View className="flex-1 ml-1 h-20 rounded-2xl flex-col justify-between">
                 <View className={'w-full flex-row justify-between pr-4 pt-1'}>
-                    <Text className={'text-xl font-semibold'}>{shoppingCategory.title}</Text>
-                    <Text className={`text-xl font-bold ${shoppingCategory.category === 'income' ? 'text-emerald-500' : 'text-red-500'}`}>
-                        {shoppingCategory.category === 'income' ? '+' : '-'}$100
+                    <Text className={'text-xl font-semibold'}>{category.title}</Text>
+                    <Text className={`text-xl font-bold ${category.category === 'income' ? 'text-emerald-500' : 'text-red-500'}`}>
+                        {category.category === 'income' ? '+' : '-'}$100
                     </Text>
                 </View>
 
