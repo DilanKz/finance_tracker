34570bbe964dc9b30c9b58c6d065a00bbf440e2d
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-08-02 13:58:54 +0530

    Update App.js

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import AddTransactionScreen from "./src/screens/tabs/addTransactionScreen";
 import {UserProvider} from "./src/components/context/userProvider";
+import { ToastProvider } from 'react-native-toast-notifications';
 
 const Stack = createNativeStackNavigator();
 export default function App() {
@@ -13,15 +14,17 @@ export default function App() {
     return (
         <UserProvider>
             <NavigationContainer>
-                <Stack.Navigator initialRouteName={'Loading'} screenOptions={{headerShown: false}}>
-                    <Stack.Screen name="Loading" component={LandingScreen}/>
-                    <Stack.Screen name="onBoard" component={OnboardScreen}/>
-                    <Stack.Screen name="main" component={MainScreen}/>
-                    <Stack.Screen name="Income" component={AddTransactionScreen} options={{headerShown: false}}
-                                  initialParams={{type: 'income'}}/>
-                    <Stack.Screen name="Expense" component={AddTransactionScreen} options={{headerShown: false}}
-                                  initialParams={{type: 'expense'}}/>
-                </Stack.Navigator>
+                <ToastProvider>
+                    <Stack.Navigator initialRouteName={'Loading'} screenOptions={{headerShown: false}}>
+                        <Stack.Screen name="Loading" component={LandingScreen}/>
+                        <Stack.Screen name="onBoard" component={OnboardScreen}/>
+                        <Stack.Screen name="main" component={MainScreen}/>
+                        <Stack.Screen name="Income" component={AddTransactionScreen} options={{headerShown: false}}
+                                      initialParams={{type: 'income'}}/>
+                        <Stack.Screen name="Expense" component={AddTransactionScreen} options={{headerShown: false}}
+                                      initialParams={{type: 'expense'}}/>
+                    </Stack.Navigator>
+                </ToastProvider>
             </NavigationContainer>
         </UserProvider>
     );
