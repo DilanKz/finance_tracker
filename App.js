b3853cc7bf56b190dceff28b6ba112b6c8d86d69
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-08-02 11:24:06 +0530

    Update App.js

--- App.js
+++ App.js
@@ -5,21 +5,24 @@ import MainScreen from "./src/screens/mainScreen";
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import AddTransactionScreen from "./src/screens/tabs/addTransactionScreen";
+import {UserProvider} from "./src/components/context/userProvider";
 
 const Stack = createNativeStackNavigator();
 export default function App() {
 
     return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName={'Loading'} screenOptions={{headerShown: false}}>
-                <Stack.Screen name="Loading" component={LandingScreen}/>
-                <Stack.Screen name="onBoard" component={OnboardScreen}/>
-                <Stack.Screen name="main" component={MainScreen}/>
-                <Stack.Screen name="Income" component={AddTransactionScreen} options={{headerShown: false}}
-                              initialParams={{type: 'income'}}/>
-                <Stack.Screen name="Expense" component={AddTransactionScreen} options={{headerShown: false}}
-                              initialParams={{type: 'expense'}}/>
-            </Stack.Navigator>
-        </NavigationContainer>
+        <UserProvider>
+            <NavigationContainer>
+                <Stack.Navigator initialRouteName={'Loading'} screenOptions={{headerShown: false}}>
+                    <Stack.Screen name="Loading" component={LandingScreen}/>
+                    <Stack.Screen name="onBoard" component={OnboardScreen}/>
+                    <Stack.Screen name="main" component={MainScreen}/>
+                    <Stack.Screen name="Income" component={AddTransactionScreen} options={{headerShown: false}}
+                                  initialParams={{type: 'income'}}/>
+                    <Stack.Screen name="Expense" component={AddTransactionScreen} options={{headerShown: false}}
+                                  initialParams={{type: 'expense'}}/>
+                </Stack.Navigator>
+            </NavigationContainer>
+        </UserProvider>
     );
 }
